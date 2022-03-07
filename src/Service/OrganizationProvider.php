52a381000ea0103364c4437481e6325879607224
Tobias Gross-Vogt <tgros@tugraz.at>
2022-03-07 10:27:08 +0100

    ApiException with info on http response codes

--- src/Service/OrganizationProvider.php
+++ src/Service/OrganizationProvider.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\BaseOrganizationConnectorCampusonlineBundle\Service;
 
+use Dbp\CampusonlineApi\LegacyWebService\ApiException;
 use Dbp\Relay\BaseOrganizationBundle\API\OrganizationProviderInterface;
 use Dbp\Relay\BaseOrganizationBundle\Entity\Organization;
 use Dbp\Relay\BasePersonBundle\Entity\Person;
 use Dbp\Relay\CoreBundle\Exception\ApiError;
+use Symfony\Component\HttpFoundation\Response;
 
 class OrganizationProvider implements OrganizationProviderInterface
 {
@@ -26,10 +28,12 @@ class OrganizationProvider implements OrganizationProviderInterface
      */
     public function getOrganizationById(string $identifier, array $options = []): ?Organization
     {
+        $org = null;
+
         try {
             $org = $this->orgApi->getOrganizationById($identifier, $options);
-        } catch (\Exception $e) {
-            throw new ApiError($e->getCode(), $e->getMessage());
+        } catch (ApiException $e) {
+            self::dispatchException($e, $identifier);
         }
 
         return $org;
@@ -44,7 +48,7 @@ class OrganizationProvider implements OrganizationProviderInterface
      */
     public function getOrganizationsByPerson(Person $person, string $context, array $options = []): array
     {
-        return [];
+        throw new ApiError(Response::HTTP_INTERNAL_SERVER_ERROR, 'query not available');
     }
 
     /**
@@ -56,8 +60,29 @@ class OrganizationProvider implements OrganizationProviderInterface
     {
         try {
             return $this->orgApi->getOrganizations($options);
-        } catch (\Exception $e) {
-            throw new ApiError($e->getCode(), $e->getMessage());
+        } catch (ApiException $e) {
+            throw new ApiError(Response::HTTP_INTERNAL_SERVER_ERROR, $e->getMessage());
         }
+    }
+
+    /**
+     * NOTE: Comfortonline returns '401 unauthorized' for some ressources that are not found. So we can't
+     * safely return '404' in all cases because '401' is also returned by CO if e.g. the token is not valid.
+     */
+    private static function dispatchException(ApiException $e, string $identifier)
+    {
+        if ($e->isHttpResponseCode()) {
+            switch ($e->getCode()) {
+                case Response::HTTP_NOT_FOUND:
+                    throw new ApiError(Response::HTTP_NOT_FOUND, sprintf("Id '%s' could not be found!", $identifier));
+                    break;
+                case Response::HTTP_UNAUTHORIZED:
+                    throw new ApiError(Response::HTTP_INTERNAL_SERVER_ERROR, sprintf("Id '%s' could not be found or access denied!", $identifier));
+                    break;
+                default:
+                    break;
+            }
+        }
+        throw new ApiError(Response::HTTP_INTERNAL_SERVER_ERROR, $e->getMessage());
     }
 }
